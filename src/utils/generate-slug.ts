3e18c3a7b702de33865c656aed9cb900ca8272e1
MessiasR <messiasamaral123@gmail.com>
2024-04-04 19:28:47 -0300

    created table attendees for database

---
+++ src/utils/generate-slug.ts
@@ -0,0 +1,9 @@
+export function generateSlug(text: string): string {
+    return text
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/[^a-z0-9 -]/g, "")
+      .replace(/\s+/g, "-")
+      .toLowerCase();
+  }
+  
