62f8ca19c289fca1a7e5cda917fad692ec124aa0
MessiasR <messiasamaral123@gmail.com>
2024-04-23 20:36:29 -0300

    add table check in and route for get attendees

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -11,7 +11,16 @@ export async function getAttendeeBadge(app: FastifyInstance) {
                 params: z.object({
                     attendeeId: z.coerce.number().int(),
                 }),
-                response: {},
+                response: {
+                    200: z.object({
+                        badge: z.object({
+                            name: z.string(),
+                            email: z.string().email(),
+                            eventTitle: z.string(),
+                            checkInURL: z.string().url(),
+                        })
+                    })
+                },
             }
         }, async (request, reply) => {
             const { attendeeId } = request.params
@@ -34,7 +43,18 @@ export async function getAttendeeBadge(app: FastifyInstance) {
             if (attendee === null) {
                 throw new Error('Attendee not found.')
             }
+            
+            const baseURL = `${request.protocol}://${request.hostname}`
+
+            const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL)
     
-            return reply.send({ attendee })
+            return reply.send({ 
+                badge: {
+                    name: attendee.name,
+                    email: attendee.email,
+                    eventTitle: attendee.event.title,
+                    checkInURL: checkInURL.toString(),
+                }
+             })
         })
 }
